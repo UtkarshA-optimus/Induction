646088daa87855c9e9eeb3556913feb8a5a39a40
UtkarshA-optimus <utkarsh.awasthi@optimusinfo.com>
2019-06-26 10:53:04 +0530

    updating code

--- Movie/src/constants.ts
+++
@@ -1,8 +0,0 @@
-export class AppConstants {
-  public static upcomingMoviesUrl: string =
-    "https://api.themoviedb.org/3/movie/upcoming?api_key=90dcc7eda423868e89cb0c4d943d2f17";
-  public static playingNowMoviesUrl: string =
-    "https://api.themoviedb.org/3/movie/now_playing?api_key=90dcc7eda423868e89cb0c4d943d2f17";
-  public static searchMovieUrl: string =
-    " http://www.omdbapi.com/?i=tt3896198&apikey=a0af53b6&";
-}
